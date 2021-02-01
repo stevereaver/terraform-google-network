b8ba598264610817f550eb5c496e49b25026196c
Stephen Bancroft <stevereaver@gmail.com>
2021-02-01 15:01:03 +1100

    Update variables.tf
    Added module_depends_on var

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -32,3 +32,9 @@ variable "secondary_ranges" {
   description = "Secondary ranges that will be used in some of the subnets"
   default     = {}
 }
+   
+variable "module_depends_on" {
+  description = "List of modules or resources this module depends on."
+  type        = list(any)
+  default     = []
+}
